56f591f56716d4b3498aa5f5311c7c88ff21a56e
PETER IRUNGU <peteralidante254@gmail.com>
2023-08-15 10:20:30 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useSpring, animated } from 'react-spring';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/About';
 import Projects from './components/Projects';
 import Music from './components/Music';
 import Home from './components/Home';
-import "./App.css"
+import './App.css';
 
 const App = () => {
+  const backgroundAnimation = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { duration: 1000 },
+  });
+
   return (
     <Router>
-      <div className="container-fluid bg-dark text-light">
+      <animated.div style={backgroundAnimation} className="container-fluid bg-dark text-light">
         <Header />
 
         <main>
@@ -24,10 +31,9 @@ const App = () => {
         </main>
 
         <Footer />
-      </div>
+      </animated.div>
     </Router>
   );
 };
-
 
 export default App;
