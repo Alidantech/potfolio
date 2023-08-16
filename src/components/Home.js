166002097f91bd724e2968b012960ff2897253e8
PETER IRUNGU <peteralidante254@gmail.com>
2023-08-16 16:13:38 +0300

    responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,46 +2,43 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="container">
-      <section>
+    <div className="container-fluid bg-light">
+      <section className="py-5">
         <div className='row'>
-          <div className="m-5 p-5 col">
-            <h2 className="p-5">PETER IRUNGU</h2>
-            <p className="lead pb-5 mt-5">Welcome to my portfolio website!</p>
+          <div className="col-lg-6 m-5 p-5">
+            <h2 className="display-4 mb-4">PETER IRUNGU</h2>
+            <p className="lead mb-5">Welcome to my portfolio website!</p>
           </div>
-          <div className='col p-5'>
-            <img src='bg.jpg' className='img-responsive border' alt='profile'></img>
+          <div className='col-lg-4 col-md-6 p-5'>
+            <img src='bg.jpg' className='img-fluid rounded border' alt='profile'></img>
           </div>
-        </div>
-        <div className='row text-center'>
-          
         </div>
       </section>
-      <section>
-        <div className="pb-5 mt-5">
-          <h3>About Me</h3>
-          <p>I am a passionate developer with experience in web development. I have expertise in the the following fields </p>
-          <ul>
+      <section className="py-5">
+        <div className="text-center">
+          <h3 className="mb-4">About Me</h3>
+          <p className="lead">I am a passionate developer with experience in web development. I have expertise in the following fields:</p>
+          <ul className="list-unstyled">
             <li>Data Science</li>
             <li>Web Development</li>
-            <li>Object Oriented Programming</li>
+            <li>Object-Oriented Programming</li>
           </ul>
-          <p>I find please in developing life changing tech solutions. You can find more information about my career journey <a href="/about">here</a></p>
+          <p>I find pleasure in developing life-changing tech solutions. You can find more information about my career journey <a href="/about">here</a>.</p>
         </div>
       </section>
-      <section>
-        <div className="pb-5 mt-5">
-          <h3>Projects</h3>
-          <p>I have worked on many open source projects. On top of that I have created apps such as potfolios and chat websites. Check out some of my recent projects<a href="/projects"> here</a>.</p>
+      <section className="py-5">
+        <div className="text-center">
+          <h3 className="mb-4">Projects</h3>
+          <p>I have worked on many open source projects. Additionally, I have created apps such as portfolios and chat websites. Check out some of my recent projects <a href="/projects">here</a>.</p>
         </div>
       </section>
-      <section>
-        <div className="pb-5 mt-5">
-          <h3>Music</h3>
-          <p>My hobby being listening to relaxing and educative music. I also like singing and have recorded some bangers. If you would like to listen to some of my recorded songs you can find them <a href="/music"> here</a>. also dont forget to like if you find it interesting.</p>
+      <section className="py-5">
+        <div className="text-center">
+          <h3 className="mb-4">Music</h3>
+          <p>My hobby is listening to relaxing and educative music. I also enjoy singing and have recorded some great songs. If you'd like to listen to some of my recorded songs, you can find them <a href="/music">here</a>. Don't forget to give a thumbs up if you find them interesting!</p>
         </div>
       </section>
-      </div>
+    </div>
   );
 };
 
