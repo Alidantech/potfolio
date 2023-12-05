fa77bff59f07f5ab34a8112477c631e81aa7635e
PETER IRUNGU <peteralidante254@gmail.com>
2023-12-05 13:39:43 +0300

    updates

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,11 +1,106 @@
+import { InfoCard } from "../styled/cards/InfoCard.styled";
+import { FlexRows } from "../styled/containers/FlexRows.styled";
+import { InfoBox } from "../styled/containers/InfoBox.styled";
+import { InfoList } from "../styled/containers/InfoList.syled";
+import { ListItem } from "../styled/containers/ListItem.styled";
+import { ProfileBox } from "../styled/containers/ProfileBox.styled";
+import { SkillsBox } from "../styled/containers/SkillsBox.styled";
+import { ProfileImage } from "../styled/images/ProfileImage.syled";
+import { DecoratedUnderline } from "../styled/items/DecoratedUnderline.styled";
+import { PersonalInfo } from "../styled/sections/PesonalInfo.styled";
+import { Heading2 } from "../styled/text/Heading2.styled";
+import { InfoData } from "../styled/text/InfoData.styled";
+import { Paragraph } from "../styled/text/Paragraph.styled";
+import { FullProgressBar, ProgressBar } from "../styled/widgets/ProgressBar.styled";
 
 
 function About() {
   
   return (
-    <>
-      <h1>about page</h1>
-    </>
+    <PersonalInfo>
+      <InfoCard>
+        <InfoBox>
+          <ProfileBox>
+            <ProfileImage src="https://picsum.photos/300/300" />
+            <InfoList>
+              <ListItem>
+                <InfoData>Peter Irungu</InfoData>
+              </ListItem>
+              <ListItem>
+                <InfoData>Software Developer</InfoData>
+              </ListItem>
+              <ListItem>
+                <InfoData>Data Scientist</InfoData>
+              </ListItem>
+              <ListItem>
+                <InfoData>Web Developer</InfoData>
+              </ListItem>
+            </InfoList>
+          </ProfileBox>
+          <SkillsBox>
+            <Heading2>Skills & Languages</Heading2>
+
+            <Paragraph>Java</Paragraph>
+            <FullProgressBar>
+              <ProgressBar
+                width={98}
+                customstyles="position: inherit; height: 15px; background-color: rgb(7, 133, 172);"
+              />
+            </FullProgressBar>
+
+            <Paragraph>Python</Paragraph>
+            <FullProgressBar>
+              <ProgressBar
+                width={75}
+                customstyles="position: inherit; height: 15px; background-color: rgb(7, 133, 172);"
+              />
+            </FullProgressBar>
+
+            <Paragraph>Typescript/Js</Paragraph>
+            <FullProgressBar>
+              <ProgressBar
+                width={95}
+                customstyles="position: inherit; height: 15px; background-color: rgb(7, 133, 172);"
+              />
+            </FullProgressBar>
+
+            <Paragraph>Dart</Paragraph>
+            <FullProgressBar>
+              <ProgressBar
+                width={89}
+                customstyles="position: inherit; height: 15px; background-color: rgb(7, 133, 172);"
+              />
+            </FullProgressBar>
+          </SkillsBox>
+        </InfoBox>
+        <InfoBox>
+          <Heading2>About me</Heading2>
+          <DecoratedUnderline />
+          <FlexRows>
+            <Paragraph>
+              Hello, my name is Peter. I am a passionate web developer and
+              software engineer based in Kenya. With a strong foundation in
+              programming and a keen eye for design, I strive to create engaging
+              and user-friendly web applications.
+            </Paragraph>
+
+            <Paragraph>
+              My journey in the world of technology began on 2021. Over the
+              years, I have gained expertise in Web Development, allowing me to
+              tackle a variety of projects with confidence and efficiency.
+            </Paragraph>
+
+            <Paragraph>
+              When I'm not coding, you can find me exploring new technologies,
+              contributing to open-source projects, or enjoying some quality
+              time with my Music and Reading. I believe in continuous learning
+              and am always excited to take on new challenges in the
+              ever-evolving landscape of web development.
+            </Paragraph>
+          </FlexRows>
+        </InfoBox>
+      </InfoCard>
+    </PersonalInfo>
   );
 }
 
--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,12 +1,138 @@
+import { IconButton } from "../styled/buttons/IconButton.styled";
+import { StackCard, StackCardDetails, StackCardInfo } from "../styled/cards/StackCard.styled";
+import { FlexColumns } from "../styled/containers/FlexColumns.styled";
+import { FlexRows } from "../styled/containers/FlexRows.styled";
+import { SpacedColumns } from "../styled/containers/SpacedColumns.styled";
+import { ButtonIcon } from "../styled/icons/ButtonIcon.styled";
+import { ImageBox, StackCardImage } from "../styled/images/StackCardImage.styled";
+import { DecoratedUnderline } from "../styled/items/DecoratedUnderline.styled";
+import { Stacks } from "../styled/sections/Stacks.styled";
+import { Heading1 } from "../styled/text/Heading1.styled";
+import { Heading2 } from "../styled/text/Heading2.styled";
+import { Paragraph } from "../styled/text/Paragraph.styled";
 
 
 
 function Projects() {
  
   return (
-    <>
-      <h1>projects page</h1>
-    </>
+    <Stacks>
+      <FlexRows>
+        <Heading1>Completed Projects</Heading1>
+        <Paragraph>In the last two years</Paragraph>
+        <DecoratedUnderline />
+        <FlexColumns>
+          <StackCard>
+            <ImageBox>
+              <StackCardImage src="https://picsum.photos/980" />
+            </ImageBox>
+            <StackCardDetails>
+              <SpacedColumns>
+                <StackCardInfo>
+                  <Heading2>Organizer</Heading2>
+                  <p>
+                    <a href="">Mobile App</a> / 18 Jun. 2023
+                  </p>
+                </StackCardInfo>
+                <IconButton>
+                  <ButtonIcon className="fas fa-arrow-right" />
+                </IconButton>
+              </SpacedColumns>
+            </StackCardDetails>
+          </StackCard>
+          <StackCard>
+            <ImageBox>
+              <StackCardImage src="https://picsum.photos/1080" />
+            </ImageBox>
+            <StackCardDetails>
+              <SpacedColumns>
+                <StackCardInfo>
+                  <Heading2>Dev Auto</Heading2>
+                  <p>
+                    <a href="">Dev Tool</a> / 12 Jan. 2022
+                  </p>
+                </StackCardInfo>
+                <IconButton>
+                  <ButtonIcon className="fas fa-arrow-right" />
+                </IconButton>
+              </SpacedColumns>
+            </StackCardDetails>
+          </StackCard>
+          <StackCard>
+            <ImageBox>
+              <StackCardImage src="https://picsum.photos/1180" />
+            </ImageBox>
+            <StackCardDetails>
+              <SpacedColumns>
+                <StackCardInfo>
+                  <Heading2>Webchat</Heading2>
+                  <p>
+                    <a href="">Website</a> / 10 Mar. 2023
+                  </p>
+                </StackCardInfo>
+                <IconButton>
+                  <ButtonIcon className="fas fa-arrow-right" />
+                </IconButton>
+              </SpacedColumns>
+            </StackCardDetails>
+          </StackCard>
+          <StackCard>
+            <ImageBox>
+              <StackCardImage src="https://picsum.photos/900" />
+            </ImageBox>
+            <StackCardDetails>
+              <SpacedColumns>
+                <StackCardInfo>
+                  <Heading2>Upcoming AI</Heading2>
+                  <p>
+                    <a href="">Data Science</a> / 20 Feb. 2023
+                  </p>
+                </StackCardInfo>
+                <IconButton>
+                  <ButtonIcon className="fas fa-arrow-right" />
+                </IconButton>
+              </SpacedColumns>
+            </StackCardDetails>
+          </StackCard>
+          <StackCard>
+            <ImageBox>
+              <StackCardImage src="https://picsum.photos/1800" />
+            </ImageBox>
+            <StackCardDetails>
+              <SpacedColumns>
+                <StackCardInfo>
+                  <Heading2>Sprianto IQ</Heading2>
+                  <p>
+                    <a href="">Website</a> / 13 Jul. 2023
+                  </p>
+                </StackCardInfo>
+                <IconButton>
+                  <ButtonIcon className="fas fa-arrow-right" />
+                </IconButton>
+              </SpacedColumns>
+            </StackCardDetails>
+          </StackCard>
+          <StackCard>
+            <ImageBox>
+              <StackCardImage src="https://picsum.photos/1000" />
+            </ImageBox>
+            <StackCardDetails>
+              <SpacedColumns>
+                <StackCardInfo>
+                  <Heading2>Farm AI</Heading2>
+                  <p>
+                    <a href="">Data Science</a> / 7 Nov. 2023
+                  </p>
+                </StackCardInfo>
+                <IconButton>
+                  <ButtonIcon className="fas fa-arrow-right" />
+                </IconButton>
+              </SpacedColumns>
+            </StackCardDetails>
+          </StackCard>
+        </FlexColumns>
+      </FlexRows>
+    </Stacks>
   );
 
 }
